f30203ff62cb9afeb1faea9737076d07afb72cb7
emi agbozu <133529260+Omomoemi@users.noreply.github.com>
2023-07-24 12:41:25 +0100

    Update size.c

--- size.c
+++ size.c
@@ -2,7 +2,7 @@
 
 /**
  * get_size - Calculates the size to cast the argument
- * by Staneley and Emi
+ * by Emi and Stanley
  * @format: Formatted string in which to print the arguments
  * @i: List of arguments to be printed.
  *
