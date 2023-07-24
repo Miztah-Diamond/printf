bdb70a750fb8c584ae98b6f3b078b6bd663b490d
emi agbozu <133529260+Omomoemi@users.noreply.github.com>
2023-07-24 12:40:04 +0100

    Update print_functions1.c

--- print_functions1.c
+++ print_functions1.c
@@ -3,7 +3,7 @@
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
  * print_unsigned - Prints an unsigned number
- * by Stanley and Emi
+ * by Emi and Stanley
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
