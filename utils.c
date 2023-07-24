46c5ef0a408fc3a34aad70b6c37ae8d86b4a9d4d
emi agbozu <133529260+Omomoemi@users.noreply.github.com>
2023-07-24 12:42:09 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -2,7 +2,7 @@
 
 /**
  * is_printable - Evaluates if a char is printable
- * by Stanley and Emi
+ * by Emi and Stanley
  * @c: Char to be evaluated.
  *
  * Return: 1 if c is printable, 0 otherwise
